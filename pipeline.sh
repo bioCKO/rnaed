58b657c10fea5e2c2c0aa175d566ea1a0e16400f
Oscar <rand@Oscars-MacBook-Pro.local>
2016-12-12 11:25:30 -0500

    updates

--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo RNA editing pipeline
+echo Downloading the human genome GRCh38
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+
+echo Uncompressing.
+gunzip GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
